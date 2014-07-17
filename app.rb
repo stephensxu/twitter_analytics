2dee1cf48778fade8043cf0780026a1b8fd86ae9
Stephens Xu <stephensxub@gmail.com>
2014-07-16 22:11:12 -0700

    deleted get timeline feature

--- app.rb
+++ app.rb
@@ -14,30 +14,6 @@ def show_params
   p "params are #{params}"
 end
 
-def tweets(screen_name)
-  authorization_header = SimpleOAuth::Header.new("get",
-                                                 "https://api.twitter.com/1.1/statuses/user_timeline.json",
-                                                 { :screen_name => screen_name },
-                                                 { :consumer_key => ENV['TWITTER_API_KEY'],
-                                                   :consumer_secret => ENV['TWITTER_API_SECRET'] })
-
-  response = Excon.send("get", "https://api.twitter.com/1.1/statuses/user_timeline.json", {
-    :query => { :screen_name => screen_name },
-    :headers => { "Authorization" => authorization_header.to_s }
-  })
-
-  response = JSON.parse(response.body)
-  if response.respond_to?(:has_key?) && response.has_key?("errors")
-    messages = []
-    response["errors"].each do |error|
-      messages.push(error["message"])
-    end
-    raise "Oops, errors! #{messages.join("\n")}"
-  else
-    return response
-  end
-end
-
 def tweets_hashtag(hash_tag)
   authorization_header = SimpleOAuth::Header.new("get",
                                                  "https://api.twitter.com/1.1/search/tweets.json",
@@ -65,18 +41,6 @@ end
 get('/') do
   @tweets = []
   erb :home
-end
-
-get('/tweets') do
-  show_params
-  if params[:screen_name]
-    @tweets = tweets(params[:screen_name])
-    puts @tweets.class
-    erb(:home)
-  else
-    @tweets = []
-    redirect("/")
-  end
 end
 
 get('/tweets_hashtag') do
